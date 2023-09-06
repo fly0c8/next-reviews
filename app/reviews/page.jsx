1be06d23cb24e598fb847ae39632cacd882edd86
arnold angerer <arnold.angerer@skylinx.aero>
2023-09-06 22:09:06 +0200

    responsive design

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -5,18 +5,18 @@ export default function ReviewsPage() {
         <>
             <Heading>Reviews</Heading>
             <ul className="flex flex-col gap-3">
-            <li className="bg-white border w-80 shadow hover:shadow-xl">
+                <li className="bg-white border w-80 shadow hover:shadow-xl">
                     <Link href="/reviews/hollow-knight">
-                    <img src="/images/hollow-knight.jpg" alt="" width="320" height="180" className="rounded-t mb-2"/>
-                     <h2 className="font-semibold font-orbitron py-1 text-center">Hollow Knight</h2>
+                        <img src="/images/hollow-knight.jpg" alt="" width="320" height="180" className="rounded-t mb-2" />
+                        <h2 className="font-semibold font-orbitron py-1 text-center">Hollow Knight</h2>
                     </Link>
-                </li>                
+                </li>
                 <li className="bg-white border w-80 shadow hover:shadow-xl">
                     <Link href="/reviews/stardew-valley">
-                    <img src="/images/stardew-valley.jpg" alt="" width="320" height="180" className="rounded-t mb-2"/>
-                     <h2 className="font-semibold font-orbitron py-1 text-center">Stardew Valley</h2>
+                        <img src="/images/stardew-valley.jpg" alt="" width="320" height="180" className="rounded-t mb-2" />
+                        <h2 className="font-semibold font-orbitron py-1 text-center">Stardew Valley</h2>
                     </Link>
-                </li>                
+                </li>
             </ul>
         </>
     );
